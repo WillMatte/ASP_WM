eb36efb19c8901f150df309a3b5c9bbeee6fc4af
WillMatte <williamatte@gmail.com>
2024-11-20 11:05:46 -0500

    Added the base database connection

--- Api/app.js
+++ Api/app.js
@@ -1,15 +1,21 @@
 ﻿const express = require('express')
 const app = express()
 const config = require('./config')
-const {PORT} = require("./config");
+const db = require('./db')
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
+app.get('/', async (req, res) => {
+    try {
+        await db.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+    res.send("Hello world")
 })
 
 app.listen(config.PORT, () => {
-    console.log(`Node.js est à l\'écoute sur le port ${PORT}`)
+    console.log(`Node.js est à l\'écoute sur le port ${config.PORT}`)
 })
 
 module.exports = app
--- Api/config.js
+++ Api/config.js
@@ -2,5 +2,9 @@
 
 module.exports = {
     PORT : process.env.PORT,
-    URL : process.env.URL
+    URL : process.env.URL,
+    DB_NAME : process.env.DB_NAME,
+    DB_USER : process.env.DB_USER,
+    DB_PASSWORD : process.env.DB_PASSWORD,
+    DB_HOST : process.env.DB_HOST
 }
